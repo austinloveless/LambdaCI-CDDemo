be51722dd28ece114fd93b84fb62250085dc5220
Itumeleng Tlali <tlalii663@gmail.com>
2021-01-18 18:57:01 +0200

    CANARY

--- Lambda/helloWorld.js
+++ Lambda/helloWorld.js
@@ -1,3 +1,3 @@
 exports.handler =  (event, context, callback) => {
-    return callback(null, { statusCode: 200, body: "Hello World Works"});
+    return callback(null, { statusCode: 200, body: "Hello World Canary"});
 };
