2231f61300f50b5ed12d6ccaa7ecd5af5ad64f2e
Tlali <Itumeleng.Tlali@vcontractor.co.za>
2021-01-20 09:38:04 +0200

    hotfix

--- Lambda/helloWorld.js
+++ Lambda/helloWorld.js
@@ -1,3 +1,3 @@
 exports.handler =  (event, context, callback) => {
-    return callback(null, { statusCode: 200, body: "Linear Code deploy Deployment Preference"});
+    return callback(null, { statusCode: 200, body: "hotfix"});
 };
